71dda8328d683254af725896b9aa2ae1fd831fcc
Łukasz Gorzelany <llukasxx@gmail.com>
2015-06-27 18:44:59 +0200

    Refactor recipes controller to use decent_exposure, commented out recipe authorization, pundit doesn't like decent_exposure

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,7 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
 
   expose(:recipes)
-  expose(:recipe) { recipe.recipe_items.build }
+  expose(:recipe)
 
   # GET /recipes
   def index
@@ -14,23 +13,22 @@ class RecipesController < ApplicationController
 
   # GET /recipes/new
   def new
-    @recipe = Recipe.new
-    authorize @recipe
-    @recipe.recipe_items.build
-    @recipe.ingredients.build
+    #authorize recipe
+    recipe.recipe_items.build
+    recipe.ingredients.build
   end
 
   # GET /recipes/1/edit
   def edit
-    authorize @recipe
+    #authorize recipe
   end
 
   # POST /recipes
   def create
-    @recipe = Recipe.new(recipe_params)
-    @recipe.user = current_user
-    if @recipe.save
-      redirect_to @recipe, notice: 'Recipe was successfully created.'
+    recipe = Recipe.new(recipe_params)
+    recipe.user = current_user
+    if recipe.save
+      redirect_to recipe, notice: 'Recipe was successfully created.'
     else
       render :new
     end
@@ -38,9 +36,9 @@ class RecipesController < ApplicationController
 
   # PATCH/PUT /recipes/1
   def update
-    authorize @recipe
-    if @recipe.update(recipe_params)
-      redirect_to @recipe, notice: 'Recipe was successfully updated.'
+    #authorize recipe
+    if recipe.update(recipe_params)
+      redirect_to recipe, notice: 'Recipe was successfully updated.'
     else
       render :edit
     end
@@ -48,17 +46,12 @@ class RecipesController < ApplicationController
 
   # DELETE /recipes/1
   def destroy
-    authorize @recipe
-    @recipe.destroy
+    #authorize recipe
+    recipe.destroy
     redirect_to recipes_url, notice: 'Recipe was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_recipe
-      @recipe = Recipe.find(params[:id])
-    end
-
     # Only allow a trusted parameter "white list" through.
     def recipe_params
       params.require(:recipe).permit(:name, :category,
