2d4b4a332f7f3c1b881a34dddc91839088c53146
wwilyW <kuba.michalec@gmail.com>
2015-06-28 10:33:16 +0200

    Spec for cloning recipes from other sites

--- spec/services/all_recipe_com_spec.rb
+++ spec/services/all_recipe_com_spec.rb
@@ -2,15 +2,20 @@ require 'rails_helper'
 
 describe AllRecipesCom do
 
-    url = "http://www.recipe.com/huaraches-con-carnitas/"
+    url = "http://allrecipes.com/Recipe/Slow-Cooker-Balsamic-Chicken/Detail.aspx?soid=recs_recipe_1"
     let(:recipe) { AllRecipesCom.new(url) }
 
   it "should return uri" do
     expect(recipe.uri).to eq url
   end
 
-  #it "should return ingridients" do
-    #recipe.params.should include()
-  #end TODO
+  it "should return name of recipe" do
+    expect(recipe.parse[:name]).to eq "Slow Cooker Balsamic Chicken"
+  end
+
+  it "should return ingridient" do
+    expect(recipe.parse[:ingredients].first).to include( {amount:"2",
+      measure: "tablespoons", name: "olive oil"} )
+  end
 
 end
